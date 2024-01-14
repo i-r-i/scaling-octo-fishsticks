13d283c39ae678f1f969d87966baf22078778405
irie <irietheyers@gmail.com>
2024-01-14 11:08:37 +0000

    Updating the display time function

--- src.js
+++ src.js
@@ -1,6 +1,31 @@
 function display_time() {
     const time = new Date();
-    const curtime = time.toLocaleTimeString();
-    document.getElementById("date_time").textContent = curtime;
+    const targetTime = new Date();
+    e = document.getElementById("date_time");
+    e.textContent = ""
+    switch (time.getDay()) {
+        case 0:
+            e.textContent += "No school today!";
+            targetTime.setDate(targetTime.getDate() + 1)
+            targetTime.setHours(8, 45, 0, 0)
+            break;
+        case 1:
+        case 5:
+            targetTime.setHours(16, 0, 0, 0);
+            break;
+        case 2:
+        case 3:
+        case 4:
+            targetTime.setHours(17, 15, 0, 0);
+            break;
+        default:
+            targetTime.setHours(11, 0, 0, 0)
+    }
+    const change = (targetTime - time) / 1000;
+
+    const hoursDifference = Math.floor(change / (1000 * 60 * 60));
+    const minutesDifference = Math.floor((change % (1000 * 60 * 60)) / (1000 * 60));
+    const secondsDifference = Math.floor((change % (1000 * 60)) / 1000);
+    e.textContent = `${hoursDifference}:${minutesDifference}:${secondsDifference}`;
 }
 setInterval(display_time, 1000);
